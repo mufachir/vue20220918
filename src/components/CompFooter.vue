c309f76ee4cf7da6b1054406746ea2803b87258f
Mufachir Hossain <mufachirhossain@gmail.com>
2022-09-24 11:50:22 +0600

    ..added footer component..

---
+++ src/components/CompFooter.vue
@@ -0,0 +1,17 @@
+<template>
+    <section class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    Find me on: <a href="https://www.facebook.com/mufachir.page">Facebook</a>
+                    &nbsp;
+                    <a href="https://twitter.com/cleancoder85">Twitter</a>
+                    &nbsp;
+                    <a href="https://www.linkedin.com/in/mufachir/">Linkedin</a>
+                    &nbsp;
+                    <a href="https://fiverr.com/cleancoder85/">Fiverr</a>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
